f41bea20adf0e60e3991ba76c1976b27f7c24d51
Tyler Ball <tball@trapeze.com>
2011-10-17 23:37:54 -0400

    added home app

--- apps/settings.py
+++ apps/settings.py
@@ -72,10 +72,13 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    
+    # Dependencies
+    
+    # Local Apps
+    'home',
 )
 
 # A sample logging configuration. The only tangible logging
