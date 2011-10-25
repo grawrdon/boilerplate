18175b8bc8a37f0b62931e434973c2d7d53e0ce5
Tyler Ball <tball@trapeze.com>
2011-10-25 17:30:54 -0400

    starting the fabfile

---
+++ fabfile.py
@@ -0,0 +1,13 @@
+import os
+from fabric.api import *
+
+
+env.unit = "TODO"
+env.path = "~/apps/%(unit)s" % env
+env.source = "git@bitbucket.com:tylerball/%(unit)s.git" % env
+env.source_http = "https://tylerball@bitbucket.org/tylerball/%(unit)s.git" % env
+env.root_dir = os.path.abspath(os.path.dirname(__file__))
+env.django_root = os.path.join(env.root_dir, 'apps/')
+
+env.pip_requirements = ["requirements/common.txt"]
+env.pip_requirements_prod = ["requirements/prod.txt"]
