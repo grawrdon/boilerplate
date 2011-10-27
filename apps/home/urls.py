3ef372021dcd75a7179229b5371cc0f2c9ed065a
Tyler Ball <tball@trapeze.com>
2011-10-27 15:06:29 -0400

    added a test and changed the home page url to a view from direct_to_template

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
-    (r'^$', direct_to_template, {'template': 'home/index.html'}),
+    #(r'^$', direct_to_template, {'template': 'home/index.html'}),
+    url(r'^$', 'home.views.home', name="home"),
 )
